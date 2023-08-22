eeb8f05269844573fdc17c60cb8d80425f6c8711
erick-efc <80852618+erick-efc@users.noreply.github.com>
2023-08-22 02:35:47 -0300

    generalizarion of the functions, connection variables and other organizations matters

--- app.py
+++ app.py
@@ -5,15 +5,31 @@ import pymysql
 
 app = Flask(__name__)
 
+# DECLARING CONSTANTS
+UPLOAD_FOLDER = './uploads'  
+HISTORICAL_DATA_FOLDER = './historical_data'
+
 # MYSQL CONFIGURATION
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'api_user'
-app.config['MYSQL_PASSWORD'] = 'globant123' #nerver hardcode sensitive information, this is just for demonstration purpose
+app.config['MYSQL_PASSWORD'] = 'globant123' # NEVER HARDCODE SENSITIVE INFORMATION, THIS IS JUST FOR DEMONSTRATION PURPOSE
 app.config['MYSQL_DB'] = 'globant_test'
 app.config['MYSQL_CURSORCLASS'] = 'pymysql.cursors.DictCursor'
-    
+app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024  
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# CONNECTION HANDLER
+def connect_now():
+    connection = pymysql.connect(
+        host='localhost',
+        user=app.config['MYSQL_USER'],
+        password=app.config['MYSQL_PASSWORD'],
+        db=app.config['MYSQL_DB'],
+    )
+    return connection
+
 # CSV TO TABLE FUNCTION
-def insert_data_into_table(connection, table_name, headers, csv_file_path):
+def insert_data_into_table(connection, table_name, csv_file_path):
     data = []
     with open(csv_file_path, 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
@@ -23,18 +39,21 @@ def insert_data_into_table(connection, table_name, headers, csv_file_path):
         del data[0]
     data = to_null (data)
     with connection.cursor() as cursor:
+        cursor.execute(f"SHOW COLUMNS FROM {table_name}") # THIS RETRIEVES THE COLUMNS NAMES SINCE THE PROVIDED CSVs DOESN'T HAVE HEADERS
+        columns = cursor.fetchall()
+        headers = ', '.join([column[0] for column in columns]) 
         for row in data:
             id_to_insert = row[0]
             check_query = f"SELECT COUNT(*) FROM {table_name} WHERE id = %s"
             cursor.execute(check_query, (id_to_insert,))
             exists = cursor.fetchone()[0]
-            if not exists:
+            if not exists: # ONLY PROCEEDS WITH PREVIOUS ID CHECK IS FALSE
                 values_placeholder = ', '.join(['%s'] * len(row))
                 query = f"INSERT INTO {table_name} ({headers}) VALUES ({values_placeholder})"
                 cursor.execute(query, row)
     return data
 
-# EMPTY TO NULL
+# EMPTY TO NULL TREATMENT
 def to_null (data):
     for row_idx, row in enumerate(data):
         for value_idx, value in enumerate(row):
@@ -49,22 +68,12 @@ def to_null (data):
 @app.route('/api/historical_data_up')
 def historical_data_up():
     try:
-        HISTORICAL_DATA_FOLDER = './historical_data'
-        connection = pymysql.connect(
-        host='localhost',
-        user=app.config['MYSQL_USER'],
-        password=app.config['MYSQL_PASSWORD'],
-        db=app.config['MYSQL_DB'],
-        )
+        connection = connect_now()
         uptade_order = ["departments", "jobs", "hired_employees"] # HIRED_EMPLOYEES IS A CHILD TABLE IN THE SCHEMA
         for table in uptade_order:
             table_name = table
             csv_file_path = os.path.join(HISTORICAL_DATA_FOLDER, f'{table_name}.csv')
-            with connection.cursor() as cursor: # THIS RETRIEVES THE COLUMNS NAMES SINCE THE PROVIDED CSVs DOESN'T HAVE HEADERS
-                cursor.execute(f"SHOW COLUMNS FROM {table_name}")
-                columns = cursor.fetchall()
-                keys = ', '.join([column[0] for column in columns]) 
-            insert_data_into_table(connection, table_name, keys, csv_file_path)  
+            insert_data_into_table(connection, table_name, csv_file_path)  
         connection.commit() 
         return jsonify({'message': 'Data uploaded successfully'}), 200
     except Exception as e:
@@ -79,9 +88,6 @@ def historical_data_up():
 @app.route('/upload', methods=['POST'])
 def upload_file():
     try:
-        UPLOAD_FOLDER = './uploads'  
-        app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-        app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024  
         if 'file' not in request.files:
             return jsonify({'error': 'No file part'}), 400
         file = request.files['file']
@@ -100,16 +106,8 @@ def upload_file():
 @app.route('/update_db_csv', methods=['POST'])
 def update_db_csv():
     try:
-        UPLOAD_FOLDER = './uploads'  
-        app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-        app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024
-        connection = pymysql.connect(
-            host='localhost',
-            user=app.config['MYSQL_USER'],
-            password=app.config['MYSQL_PASSWORD'],
-            db=app.config['MYSQL_DB'],
-        ) 
-        if 'file' not in request.files:
+        connection = connect_now()
+        if 'file' not in request.files: # ENSURING A FILE WAS PASSED IN THE REQUEST
             return jsonify({'error': 'No file part'}), 400
         file = request.files['file']
         if file.filename == '':
@@ -119,13 +117,10 @@ def update_db_csv():
             file.save(csv_file_path)
         table_name = request.form.get('table')
         if not table_name:
-            table_name = os.path.splitext(file.filename)[0]
-        with connection.cursor() as cursor: # THIS RETRIEVES THE COLUMNS NAMES SINCE THE PROVIDED CSVs DOESN'T HAVE HEADERS
-            cursor.execute(f"SHOW COLUMNS FROM {table_name}")
-            columns = cursor.fetchall()
-            headers = ', '.join([column[0] for column in columns])
-            insert_data_into_table(connection, table_name, headers, csv_file_path)
-        connection.commit() 
+            table_name = os.path.splitext(file.filename)[0] # RETRIEVING TABLE NAME BASED ON FILE
+        insert_data_into_table(connection, table_name, csv_file_path)
+        connection.commit()
+        os.remove(csv_file_path)
         return jsonify({'message': 'Data uploaded successfully'}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 400
