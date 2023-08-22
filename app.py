2c2625af957d1c9d54b4cb7bbfbe74834bfe4c2b
erick-efc <80852618+erick-efc@users.noreply.github.com>
2023-08-21 23:14:58 -0300

    simplified update order for historical data, reorganized and cleaned version

--- app.py
+++ app.py
@@ -13,70 +13,60 @@ app.config['MYSQL_PASSWORD'] = 'globant123' #nerver hardcode sensitive informati
 app.config['MYSQL_DB'] = 'globant_test'
 app.config['MYSQL_CURSORCLASS'] = 'pymysql.cursors.DictCursor'
 
-# SPECIFY HERE THE HISTORICAL DATA
+# CSV TO TABLE FUNCTION
+def insert_data_into_table(connection, table_name, keys, csv_file_path):
+    data = []
+    with open(csv_file_path, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            data.append(row)
+    data = to_null (data)
+    with connection.cursor() as cursor:
+        for row in data:
+            id_to_insert = row[0]
+            check_query = f"SELECT COUNT(*) FROM {table_name} WHERE id = %s"
+            cursor.execute(check_query, (id_to_insert,))
+            exists = cursor.fetchone()[0]
+            if not exists:
+                values_placeholder = ', '.join(['%s'] * len(row))
+                query = f"INSERT INTO {table_name} ({keys}) VALUES ({values_placeholder})"
+                cursor.execute(query, row)
+    return data
 
-# ROUTES
+# EMPTY TO NULL
+def to_null (data):
+    for row_idx, row in enumerate(data):
+        for value_idx, value in enumerate(row):
+            if value == '':
+                data[row_idx][value_idx] = None
+    return data
 
-# Test route to retrieve data from the database
-# @app.route('/test', methods=['GET'])
-# def test_database():
-#     connection = pymysql.connect(
-#         host=app.config['MYSQL_HOST'],
-#         user=app.config['MYSQL_USER'],
-#         password=app.config['MYSQL_PASSWORD'],
-#         db=app.config['MYSQL_DB'],
-#     )
-#     cursor = connection.cursor()
-#     cursor.execute('SELECT * FROM departments')
-#     data = cursor.fetchall()
-#     cursor.close()
-#     connection.close()
-#     return jsonify(data)
-
-# ROUTE TO UPLOAD TABLES WITH HISTORICAL LOCAL CSV DATA
+################################################
+# ROUTE TO RETRIEVE TABLES WITH HISTORICAL DATA
+################################################
 @app.route('/api/historical_data_up')
 def historical_data_up():
-    connection = pymysql.connect( # CALLING CONNECT FROM PYMSQL
-        host=app.config['MYSQL_HOST'],
+    try:
+        HISTORICAL_DATA_FOLDER = './historical_data'
+        connection = pymysql.connect(
+        host='localhost',
         user=app.config['MYSQL_USER'],
         password=app.config['MYSQL_PASSWORD'],
         db=app.config['MYSQL_DB'],
-    )
-    HISTORICAL_DATA_FOLDER = './files'
-    with open('historical_tables_config.json', 'r') as config_file:
-        table_config = json.load(config_file)
-    for table_info in table_config['tables']:
-        table_name = table_info['name']
-        csv_file_path = os.path.join(HISTORICAL_DATA_FOLDER, f'{table_name}.csv') # RETRIEVING PATH FOR EACH FILE
-
-                # BUILDING THE QUERY
-        data = []
-        with connection.cursor() as cursor: # THIS RETRIEVES THE COLUMNS NAMES SINCE THE PROVIDED CSVs DOESN'T HAVE HEADERS
-            cursor.execute(f"SHOW COLUMNS FROM {table_name}")
-            columns = cursor.fetchall()
-            keys = ', '.join([column[0] for column in columns])  # Extract the first element of each tuple
-        with open(csv_file_path, 'r') as csv_file:
-            csv_reader = csv.reader(csv_file)
-            for row in csv_reader:
-                data.append(row)
-        
-        for row_idx, row in enumerate(data):
-            for value_idx, value in enumerate(row):
-                if value == '':
-                    data[row_idx][value_idx] = None
-
-        # Insert data into the corresponding table
-        with connection.cursor() as cursor:
-            for row in data:
-                id_to_insert = row[0]
-                check_query = f"SELECT COUNT(*) FROM {table_name} WHERE id = %s"
-                cursor.execute(check_query, (id_to_insert,))
-                exists = cursor.fetchone()[0]
-                if not exists:
-                    values_placeholder = ', '.join(['%s'] * len(row))
-                    query = f"INSERT INTO {table_name} ({keys}) VALUES ({values_placeholder})"
-                    cursor.execute(query, row)
-    connection.commit()  # Commit the changes to the database
-    connection.close()  # Close the connection
-
-    return "Data uploaded successfully."
+        )
+        uptade_order = ["departments", "jobs", "hired_employees"] # HIRED_EMPLOYEES IS A CHILD TABLE IN THE SCHEMA
+        for table in uptade_order:
+            table_name = table
+            csv_file_path = os.path.join(HISTORICAL_DATA_FOLDER, f'{table_name}.csv')
+            with connection.cursor() as cursor: # THIS RETRIEVES THE COLUMNS NAMES SINCE THE PROVIDED CSVs DOESN'T HAVE HEADERS
+                cursor.execute(f"SHOW COLUMNS FROM {table_name}")
+                columns = cursor.fetchall()
+                keys = ', '.join([column[0] for column in columns]) 
+            insert_data_into_table(connection, table_name, keys, csv_file_path)  
+        connection.commit() 
+        return jsonify({'message': 'Data uploaded successfully'})
+    except Exception as e:
+        connection.rollback()
+        return jsonify({'error': str(e)})
+    finally:
+        connection.close()
