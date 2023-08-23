fcb9929906a2cc3ed82380c5dc48049777720c29
erick-efc <80852618+erick-efc@users.noreply.github.com>
2023-08-23 12:26:30 -0300

    new sanitize logic

--- not_working.py
+++ not_working.py
@@ -1,16 +1,3 @@
-import csv
-import pymysql
-from flask import jsonify, current_app as app
-
-# CONNECTION HANDLER
-def connect_now():
-    connection = pymysql.connect(
-        host='localhost',
-        user=app.config['MYSQL_USER'],
-        password=app.config['MYSQL_PASSWORD'],
-        db=app.config['MYSQL_DB'],
-    )
-    return connection
 
 # CSV TO TABLE FUNCTION
 def insert_data_into_table(connection, table_name, csv_file_path):
@@ -18,17 +5,10 @@ def insert_data_into_table(connection, table_name, csv_file_path):
     with open(csv_file_path, 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
         for row in csv_reader:
-            sanitized_row = []
-            for value in row:
-                if value == "":
-                    sanitized_row.append(None)
-                elif value.isdigit():
-                    sanitized_row.append(int(value))
-                else:
-                    sanitized_row.append(value)
-            data.append(sanitized_row)
-    # if all(value.isalpha() for value in data[0]):
-    #     del data[0]
+            data.append(row)
+    if all(value.isalpha() for value in data[0]):
+        del data[0]
+    data = to_null (data)
     try:
         with connection.cursor() as cursor:
             cursor.execute(f"SHOW COLUMNS FROM {table_name}") # THIS RETRIEVES THE COLUMNS NAMES SINCE THE PROVIDED CSVs DOESN'T HAVE HEADERS
@@ -42,10 +22,19 @@ def insert_data_into_table(connection, table_name, csv_file_path):
                 if not exists: # ONLY PROCEEDS WITH PREVIOUS ID CHECK IS FALSE
                     values_placeholder = ', '.join(['%s'] * len(row))
                     query = f"INSERT INTO {table_name} ({headers}) VALUES ({values_placeholder})"
-                    cursor.execute(query, row)
+                    row_values = [int(value) if value.isdigit() else value for value in row]
+                    cursor.execute(query, row_values)
         return jsonify({'message': 'Data inserted successfully'}), 201
     except Exception as e:
         return jsonify({'error': str(e)}), 400
+
+# EMPTY TO NULL TREATMENT
+def to_null (data):
+    for row_idx, row in enumerate(data):
+        for value_idx, value in enumerate(row):
+            if value == '':
+                data[row_idx][value_idx] = None
+    return data
 
 # TO SORT BASED ON HIERARCHY OF OUR GB
 def sort(update_order, target_list):
@@ -53,3 +42,28 @@ def sort(update_order, target_list):
     sorted_target_list = sorted(target_list, key=lambda x: position_map.get(x, float('inf')))
     return sorted_target_list
 
+@extra_routes_bp.route('/api/historical_data_bkup_feed')
+def historical_data_up():
+    try:
+        connection = connect_now()
+        update_order = ["departments", "jobs", "hired_employees"] # HIRED_EMPLOYEES IS A CHILD TABLE IN THE SCHEMA
+        for table in update_order:
+            table_name = table
+            csv_file_path = os.path.join(HISTORICAL_DATA_FOLDER, f'{table_name}.csv')
+            insert_data_into_table(connection, table_name, csv_file_path)  
+        connection.commit() 
+        return jsonify({'message': 'Data retrieved successfully'}), 200
+    except Exception as e:
+        connection.rollback()
+        return jsonify({'error': str(e)}), 400
+    finally:
+        connection.close()
+
+        def connect_now():
+    connection = pymysql.connect(
+        host='localhost',
+        user=app.config['MYSQL_USER'],
+        password=app.config['MYSQL_PASSWORD'],
+        db=app.config['MYSQL_DB'],
+    )
+    return connection
