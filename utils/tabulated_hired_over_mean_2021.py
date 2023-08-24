d2527fe6fcfbf15dfdf4f34b1b6b42d90e3f591b
erick-efc <80852618+erick-efc@users.noreply.github.com>
2023-08-24 10:20:19 -0300

    tabulated queries now pointing to the new server

--- utils/tabulated_hired_over_mean_2021.py
+++ utils/tabulated_hired_over_mean_2021.py
@@ -1,7 +1,7 @@
 import requests
 from tabulate import tabulate
 
-url = 'http://localhost:5000/api/sql/hired_over_mean_2021'
+url = 'http://ec2-54-226-246-113.compute-1.amazonaws.com/api/sql/hired_over_mean_2021'
 
 response = requests.get(url)
 table = response.json()  # The response should already be the tabulated data as a string
